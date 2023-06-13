a7f109ffb2ffc86321909ebc520a3f395b349e5d
Adriswara <chevellerz454@yahoo.com>
2023-06-13 19:16:08 +0700

    sql

--- student_portal.sql
+++ student_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 11, 2023 at 05:04 AM
+-- Generation Time: Jun 13, 2023 at 12:15 PM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -29,17 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `dosen` (
   `id_dosen` int NOT NULL,
-  `nama_dosen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+  `nama_dosen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `soft_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `dosen`
 --
 
-INSERT INTO `dosen` (`id_dosen`, `nama_dosen`) VALUES
-(1, 'lorem'),
-(2, 'ipsum'),
-(3, 'Dolors');
+INSERT INTO `dosen` (`id_dosen`, `nama_dosen`, `soft_delete`) VALUES
+(1, 'lorem', 0),
+(2, 'ipsum', 0),
+(3, 'Dolors', 0),
+(4, 'soft delete', 1);
 
 -- --------------------------------------------------------
 
@@ -51,15 +53,17 @@ CREATE TABLE `informasi_perkuliahan` (
   `id_informasi` int NOT NULL,
   `judul` varchar(100) NOT NULL,
   `tanggal` date NOT NULL,
-  `isiPengumuman` varchar(255) NOT NULL
+  `isiPengumuman` varchar(255) NOT NULL,
+  `informasi_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `informasi_perkuliahan`
 --
 
-INSERT INTO `informasi_perkuliahan` (`id_informasi`, `judul`, `tanggal`, `isiPengumuman`) VALUES
-(1, 'dipindahh', '2023-06-07', 'lalalala');
+INSERT INTO `informasi_perkuliahan` (`id_informasi`, `judul`, `tanggal`, `isiPengumuman`, `informasi_delete`) VALUES
+(1, 'dipindahh', '2023-06-07', 'lalalala', 0),
+(2, 'Berisi', '2023-06-06', 'Bahkan', 1);
 
 -- --------------------------------------------------------
 
@@ -74,15 +78,18 @@ CREATE TABLE `jadwal` (
   `shift_kelas` varchar(255) NOT NULL,
   `hari` varchar(255) NOT NULL,
   `jam` time NOT NULL,
-  `lokasi` varchar(255) NOT NULL
+  `lokasi` varchar(255) NOT NULL,
+  `periode_akademik` varchar(255) NOT NULL,
+  `jadwal_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `fk_matakuliah`, `fk_dosen`, `shift_kelas`, `hari`, `jam`, `lokasi`) VALUES
-(1, 1, 1, 'A1', 'senin', '11:11:11', 'J301');
+INSERT INTO `jadwal` (`id_jadwal`, `fk_matakuliah`, `fk_dosen`, `shift_kelas`, `hari`, `jam`, `lokasi`, `periode_akademik`, `jadwal_delete`) VALUES
+(1, 1, 1, 'A1', 'senin', '11:11:11', 'J301', 'Genap 2022', 0),
+(2, 2, 1, 'B3', 'Senin', '19:00:00', 'Bandung', 'Ganjil 2023', 1);
 
 -- --------------------------------------------------------
 
@@ -97,17 +104,20 @@ CREATE TABLE `mahasiswa` (
   `ipk` double NOT NULL,
   `sks_lulus` int NOT NULL,
   `password` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `superuser` int NOT NULL DEFAULT '0',
+  `mahasiswa_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id_mahasiswa`, `nama`, `role`, `ipk`, `sks_lulus`, `password`, `username`) VALUES
-(1, 'Doe', 'A1', 4, 144, '$2y$10$6Td36/72GdlKKOhBkjJkLOaSxO1dxlZ/YkePR70Nz2POML4Oon.na', 'doe'),
-(2, 'john', '4', 2, 22, '$2y$10$4A2Z5yV4ij8iovUYOVY12.vNj/BSBRjvI/IejhQyAeFJ0uoCs5lfy', 'john'),
-(3, 'admin', 'admin', 5, 999, '$2y$10$.dd129YzHQyeBnVeKGCJKuL4v.trqTI6HBBgM0G5LfaFnwRRM47sa', 'admin');
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `nama`, `role`, `ipk`, `sks_lulus`, `password`, `username`, `superuser`, `mahasiswa_delete`) VALUES
+(1, 'Doe', 'A1', 4, 144, '$2y$10$6Td36/72GdlKKOhBkjJkLOaSxO1dxlZ/YkePR70Nz2POML4Oon.na', 'doe', 0, 0),
+(2, 'john', 'A2', 2, 22, '$2y$10$4A2Z5yV4ij8iovUYOVY12.vNj/BSBRjvI/IejhQyAeFJ0uoCs5lfy', 'john', 0, 0),
+(3, 'admin', 'admin', 5, 999, '$2y$10$.dd129YzHQyeBnVeKGCJKuL4v.trqTI6HBBgM0G5LfaFnwRRM47sa', 'admin', 1, 0),
+(5, 'h', 'h', 1, 1, '$2y$10$jZFzc5W2gdyQyf9024bmmuQPRJZU5JNgG3T1PPqbU2KrwUo6fhG9q', 's', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -119,29 +129,18 @@ CREATE TABLE `matakuliah` (
   `id_matakuliah` int NOT NULL,
   `nama_kuliah` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `sks` int NOT NULL,
-  `fk_dosen` int NOT NULL DEFAULT '0'
+  `fk_dosen` int NOT NULL DEFAULT '0',
+  `matakuliah_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `matakuliah`
 --
 
-INSERT INTO `matakuliah` (`id_matakuliah`, `nama_kuliah`, `sks`, `fk_dosen`) VALUES
-(1, 'pbo', 18, 1),
-(2, 'strukdat', 6, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pengumuman`
---
-
-CREATE TABLE `pengumuman` (
-  `id_pengumuman` int NOT NULL,
-  `judul` varchar(200) NOT NULL,
-  `tanggal` date NOT NULL,
-  `isi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `matakuliah` (`id_matakuliah`, `nama_kuliah`, `sks`, `fk_dosen`, `matakuliah_delete`) VALUES
+(1, 'pbo', 18, 1, 0),
+(2, 'strukdat', 6, 2, 0),
+(3, 'Jaringan Komedi', 6, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -152,27 +151,33 @@ CREATE TABLE `pengumuman` (
 CREATE TABLE `transaksi_matakuliah` (
   `id_transaksi` int NOT NULL,
   `fk_mahasiswa` int NOT NULL,
-  `fk_perkuliahan` int NOT NULL
+  `fk_jadwal` int NOT NULL,
+  `transaksi_delete` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `transaksi_matakuliah`
 --
 
-INSERT INTO `transaksi_matakuliah` (`id_transaksi`, `fk_mahasiswa`, `fk_perkuliahan`) VALUES
-(1, 1, 1),
-(2, 2, 1),
-(5, 0, 0),
-(6, 2, 0),
-(7, 0, 0),
-(8, 2, 0),
-(9, 0, 0),
-(10, 2, 0),
-(11, 0, 0),
-(12, 1, 2),
-(13, 1, 2),
-(14, 2, 2),
-(15, 1, 2);
+INSERT INTO `transaksi_matakuliah` (`id_transaksi`, `fk_mahasiswa`, `fk_jadwal`, `transaksi_delete`) VALUES
+(1, 1, 1, 0),
+(2, 2, 0, 0),
+(5, 0, 0, 0),
+(6, 2, 0, 0),
+(7, 0, 0, 0),
+(8, 2, 0, 0),
+(9, 0, 0, 0),
+(10, 2, 0, 0),
+(11, 0, 0, 0),
+(12, 1, 0, 0),
+(13, 1, 0, 0),
+(14, 2, 0, 0),
+(15, 1, 0, 0),
+(16, 3, 0, 0),
+(17, 1, 0, 0),
+(18, 2, 0, 0),
+(19, 2, 0, 0),
+(20, 2, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -209,12 +214,6 @@ ALTER TABLE `matakuliah`
   ADD PRIMARY KEY (`id_matakuliah`);
 
 --
--- Indexes for table `pengumuman`
---
-ALTER TABLE `pengumuman`
-  ADD PRIMARY KEY (`id_pengumuman`);
-
---
 -- Indexes for table `transaksi_matakuliah`
 --
 ALTER TABLE `transaksi_matakuliah`
@@ -228,43 +227,37 @@ ALTER TABLE `transaksi_matakuliah`
 -- AUTO_INCREMENT for table `dosen`
 --
 ALTER TABLE `dosen`
-  MODIFY `id_dosen` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_dosen` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `informasi_perkuliahan`
 --
 ALTER TABLE `informasi_perkuliahan`
-  MODIFY `id_informasi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_informasi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `matakuliah`
 --
 ALTER TABLE `matakuliah`
-  MODIFY `id_matakuliah` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `pengumuman`
---
-ALTER TABLE `pengumuman`
-  MODIFY `id_pengumuman` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_matakuliah` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `transaksi_matakuliah`
 --
 ALTER TABLE `transaksi_matakuliah`
-  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
